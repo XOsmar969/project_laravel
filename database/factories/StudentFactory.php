e671e9fbca1d7beeda535c16f234beb5adbcf222
XOsmar969304 <osmarbaba234@gmail.com>
2025-10-08 10:37:38 +0700

    asesmen-web 081025

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -3,14 +3,26 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Classroom; 
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
 class StudentFactory extends Factory
 {
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
-            'classroom_id' => null, // akan diisi otomatis saat pakai hasStudents()
+            'date' => $this->faker->date('Y-m-d'), 
+            'classroom_id' => Classroom::factory(), 
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address(),
         ];
     }
 }
